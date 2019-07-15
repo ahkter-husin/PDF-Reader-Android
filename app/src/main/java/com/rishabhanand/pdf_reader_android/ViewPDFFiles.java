a56ba348846e26d682776c7f49d7593fb47f6e91
Rishabh-1999 <rishabhanand33@gmail.com>
2019-07-15 18:36:54 +0530

    Commit

---
+++ app/src/main/java/com/rishabhanand/pdf_reader_android/ViewPDFFiles.java
@@ -0,0 +1,37 @@
+package com.rishabhanand.pdf_reader_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+public class ViewPDFFiles extends AppCompatActivity {
+
+    PDFView pdfView;
+    int position = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pdffiles);
+
+        pdfView=(PDFView)findViewById(R.id.pdfView);
+
+        position=getIntent().getIntExtra("position",-1);
+
+        displayPDF();
+    }
+
+    private void displayPDF() {
+
+        pdfView.fromFile(MainActivity.pdfarray.get(position))
+                .enableSwipe(true)
+                .enableAnnotationRendering(true)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+
+
+
+    }
+}
